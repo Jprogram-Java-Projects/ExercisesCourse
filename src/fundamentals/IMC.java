a873be16a1dffc3ffd6b5a94d3aa79f2b83d7f6b
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-02-06 18:29:50 -0300

    BMI exercise

--- src/fundamentals/IMC.java
+++ src/fundamentals/IMC.java
@@ -1,5 +1,29 @@
 package fundamentals;
 
-public class IMC {
+import java.util.Scanner;
 
+/**
+ * Program to calculate IMC (BMI)
+ * 
+ * Author: Jefferson Silva
+ * Date : 06/02/2024
+ * Last modified: 06/02/2024 
+ * 
+ */
+
+public class IMC {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the height in meters: ");
+		double height = input.nextDouble();
+		
+		System.out.print("Enter the weight in kilograms: ");
+		double weight = input.nextDouble();
+		
+		System.out.println("IMC = "+ weight / (height * height));
+		
+		input.close();
+	}
 }
