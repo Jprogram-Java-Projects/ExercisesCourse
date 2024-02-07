1505c671cb223404b748e4d12f50263fb25da87e
Jefferson Gomes <jeffsilvaygo@gmail.com>
2024-02-07 15:35:42 -0300

    Exercise to calculate the area of ​​a triangle

--- src/fundamentals/Geometry.java
+++ src/fundamentals/Geometry.java
@@ -1,5 +1,33 @@
 package fundamentals;
 
-public class Geometry {
+import java.util.Scanner;
 
+/**
+ * Create a program that reads the value of the base and height of a triangle and calculates the area.
+ * 
+ * Author: Jefferson Silva
+ * Date : 07/02/2024
+ * Last modified: 07/02/2024 
+ * 
+ * Modifications:
+ * 
+ *  - v1.1.0 : Created program
+ * 
+ */
+
+public class Geometry {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the height of the triangle: ");
+		int height = input.nextInt();
+		
+		System.out.print("Enter the value of the base of the triangle: ");
+		int base = input.nextInt();
+		
+		System.out.println( "Area = " + (base * height)/2 );
+		
+		input.close();
+	}
 }
